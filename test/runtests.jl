d136dac60aa74560b31721026aff9d0adfd1dc28
jklynch <jlynch@bnl.gov>
2022-12-11 21:04:22 -0500

    add authentication test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using Test
-@testset "Test netloc" begin
 
-    @test true
+using TiledClient
+
+
+@testset "Test Password Authentication" begin
+ 
+    tiled_context = TiledClient.Context("http://127.0.0.1:8000/api/")
+    @test length(tiled_context.tokens) == 0
+
+    TiledClient.authenticate!(tiled_context, username="alice", password="secret1")
+    @test length(tiled_context.tokens) == 5
+    @test length(tiled_context.tokens["refresh_token"]) > 0
+    @test length(tiled_context.tokens["access_token"]) > 0
+    @test tiled_context.tokens["token_type"] == "bearer"
 
 end
